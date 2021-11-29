1f827293a82a8d96c9bf0a8102d37236381a6d1a
Sparrow0hawk <alexjcoleman@gmail.com>
2021-11-29 12:29:14 +0000

    converted nameform to functional component
    - implements useState to perform axios post submission for answers

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import GetLocation from './GetLocation';
 
-class NameForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { value: '' };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+function NameForm(props) {
+    const [answer, setAnswer] = useState("");
+    const [result, setResult] = useState("");
 
-    handleChange(event) { this.setState({ value: event.target.value }); }
-    handleSubmit(event) {
-        axios.post("/api/message", { 'name': this.state.value })
-            .then(response => this.setState({ result: response.data }))
+    const data = {
+        answer: answer,
+        day: GetLocation()
+    };
+
+    const handleSubmit = (event) => {
+        axios.post("/api/message", { 'answer': data.answer, 'day': data.day })
+            .then(response => setResult(response.data))
         event.preventDefault();
     }
 
-    render() {
-        return (
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Answer
+                    <input
+                        type="text"
+                        value={answer}
+                        onChange={e => setAnswer(e.target.value)}
+                    />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
             <div>
-                <form onSubmit={this.handleSubmit}>        <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />        </label>
-                    <input type="submit" value="Submit" />
-                </form>
-                <div>
-                    {this.state.result}
-                </div>
+                {result}
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default NameForm;
